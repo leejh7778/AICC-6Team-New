617e111ab7e1d3031aab85af0e8bf4f3228359bb
80sp <yjsion8085@gmail.com>
2024-08-27 09:19:50 +0900

    s.8.27

--- front/src/components/map/index.jsx
+++ front/src/components/map/index.jsx
@@ -1,38 +1,33 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
 import useGeoloaction from '../../hooks/useGeolocation';
 
-function Map(){
+function Map() {
   const mapRef = useRef(null);
-  const {naver} = window;
-  const {currentMyLocation} = useGeoloaction();
-  
+  const { naver } = window;
+  const { currentMyLocation } = useGeoloaction();
+
   useEffect(() => {
-    if (currentMyLocation.lat !== 0 && currentMyLocation.lng !== 0 ) {
-      
+    if (currentMyLocation.lat !== 0 && currentMyLocation.lng !== 0) {
       const mapOptions = {
         center: new naver.maps.LatLng(37.48097121950012, 126.8794883707286),
         logoControl: false,
         mapDataControl: false,
         scaleControl: true,
-        tileDuration:200,
-        zoom:14,
-        zoomControlOptions:{position:9},
+        tileDuration: 200,
+        zoom: 14,
+        zoomControlOptions: { position: 9 },
       };
-      mapRef.current = new naver.maps.Map(
-        'map',
-        mapOptions
-      );
+      mapRef.current = new naver.maps.Map('map', mapOptions);
       new naver.maps.Marker({
-        position: new naver.maps.LatLng(currentMyLocation.lat, currentMyLocation.lng),
+        position: new naver.maps.LatLng(
+          currentMyLocation.lat,
+          currentMyLocation.lng
+        ),
         map: mapRef.current,
       });
     }
-  },[currentMyLocation]);
-  return(
-    <div id='map' className='w-[80%] h-[500px]'/>
-  )
+  }, [currentMyLocation]);
+  return <div id="map" className="w-[80%] h-[500px]" />;
 }
 
-
-export default Map
-
+export default Map;
