d58293783f9bcccc0f8ee28eacc4927f94f1dbaf
lollipopwyn <rwewine@gmail.com>
2024-09-02 10:40:48 +0900

    JH 10:40 로그인 1차 수정

--- back/routes/getRoute.js
+++ back/routes/getRoute.js
@@ -1,8 +1,8 @@
 const router = require('express').Router(); // api path를 전달해 주는 메서드
 const { getReserv, gethosp, getInq } = require('../controllers/getTask');
 
-router.get('/get_reserv:reserv_idx', getReserv); // 컨트롤러 함수 연결 - :은 정해지지 않은 문자열 표시
-router.get('/get_inquiry:inq_idx', getInq);
-router.get('/get_inquiry:hosp_idx', gethosp);
+router.get('/get_reserv/:reserv_idx', getReserv); // 컨트롤러 함수 연결 - :은 정해지지 않은 문자열 표시
+router.get('/get_inquiry/:inq_idx', getInq);
+router.get('/get_inquiry/:hosp_idx', gethosp);
 
 module.exports = router; // router 모듈 내보내기
